ec9dbb7b390bf13ab925635c8e7ea0479ef88e29
YogeshKumar-saini <yksaini0193@gmail.com>
2025-08-30 01:18:11 +0530

    feat: implement authentication flow with login and registration components; set up routing and user context

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,57 @@
-import { Button } from "@/components/ui/button"
+import React, { useContext, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { Context } from "./main";
+import OtpVerification from "./pages/OtpVerification";
+
+// Define the context type
+interface AppContextType {
+  setIsAuthenticated: (auth: boolean) => void;
+  setUser: (user: unknown) => void;
+}
 
 function App() {
+
+  const { setIsAuthenticated, setUser } = useContext(Context) as AppContextType;
+
+  useEffect(() => {
+    const getUser = async () => {
+      await axios
+        .get("http://localhost:4000/api/v1/user/me", { withCredentials: true })
+        .then((res) => {
+          setUser(res.data.user);
+          setIsAuthenticated(true);
+        })
+        .catch(() => {
+          setUser(null);
+          setIsAuthenticated(false);
+        });
+    };
+    getUser();
+  }, []);
+
+
   return (
-    <div className="text-3xl font-bold underline h-screen">
-      <div>
-        <p>hii i am yogesh</p>
-      </div>
-      <Button>Click me</Button>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route
+            path="/otp-verification/:email/:phone"
+            element={<OtpVerification />}
+          />
+          <Route path="/password/forgot" element={<ForgotPassword />} />
+          <Route path="/password/reset/:token" element={<ResetPassword />} />
+        </Routes>
+        <ToastContainer theme="colored" />
+      </Router>
   )
 }
 
